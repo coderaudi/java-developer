3c0b66e62b7bbf0c67596857e340daacfe508f6b
ABHIJEET KHIRE <coderaudi@users.noreply.github.com>
2018-06-26 01:29:06 +0530

    Add files via upload

---
+++ Numbersprints.java
@@ -0,0 +1,12 @@
+class Numbersprints
+
+{
+	public static void main(String[] args) {
+
+		int a=100;
+
+		System.out.println(a);
+
+		System.out.println("the number we have to print is " +a);
+	}
+}
---
+++ Working.java
@@ -0,0 +1,11 @@
+class Working
+
+{
+
+public static void main(String[] args) 
+
+{
+	System.out.println("everthing is ok with the java");
+}
+
+}
